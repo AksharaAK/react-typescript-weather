e5afe5cd4065957a4deda8eca78076725a8b6063
AksharaAK <akshann1606@gmail.com>
2023-02-10 09:10:45 +0000

    feat: create a custom hook and render on app

--- starter-files/src/App.tsx
+++ starter-files/src/App.tsx
@@ -1,7 +1,23 @@
-const App = () => {
+import Search from './components/Search'
+import Forecast from './components/Forecast'
+import { useForecast } from './hooks/useForecast'
+
+const App = (): JSX.Element => {
+  const { term, options, forecast, onInputChange, onOptionSelect, onSubmit } =
+    useForecast()
   return (
-    <main className="flex justify-center items-center bg-gradient-to-br from-[#006466] via-[#212f45] to-[#4d194d] h-[100vh] w-full">
-      Hello world
+    <main className="flex justify-center items-center bg-gradient-to-br from-[#bce784] via-[#348aa7]  to-[#892b64] h-[100vh] w-full">
+      {forecast ? (
+        <Forecast data={forecast} />
+      ) : (
+        <Search
+          term={term}
+          options={options}
+          onInputChange={onInputChange}
+          onOptionSelect={onOptionSelect}
+          onSubmit={onSubmit}
+        />
+      )}
     </main>
   )
 }
